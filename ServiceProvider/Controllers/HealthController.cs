82a7588a8706174a6f0bbdb6a9f3a7fcfc7a13d0
e <18009838166@163.com>
2020-04-13 18:01:24 +0800

    Service registry

---
+++ ServiceProvider/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceProvider.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("api/health")]
+        public ActionResult healthCheck() => Ok("Ok");
+    }
+}
